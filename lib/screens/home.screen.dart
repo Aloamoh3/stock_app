057178e3ef33acc08a9bccc5bb3d0aa4f64363bf
Aloamoh3 <aloamoh@icloud.com>
2025-12-24 05:41:03 -0500

    Update home.screen.dart

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,21 +1,134 @@
 import 'package:flutter/material.dart';
-import 'dashboard.screen.dart';
+import 'package:flutter/services.dart';
+import 'package:iconsax/iconsax.dart';
+import '/utils/app.colors.dart';
+import '/widgets/balance_display.dart';
+import '/widgets/custom_app_bar.dart';
+import '/widgets/line.chart.dart';
+import '/widgets/transaction_button.dart';
+import '/widgets/transactions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const DashboardScreen(), 
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        backgroundColor: Colors.black,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.credit_card), label: "Card"),
-        ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: AppColors.darkBackground,
+        systemNavigationBarColor: AppColors.navBarBackground,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        backgroundColor: AppColors.darkBackground,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              BalanceDisplay(),
+              TransactionButtonRow(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: Text(
+                  "Featured investment",
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    spacing: 8,
+                    children: [
+                      StockDisplay(name: "Apple", symbol: "AAPL", isUp: true),
+
+                      StockDisplay(
+                        name: "Google",
+                        symbol: "GOOGL",
+                        isUp: false,
+                      ),
+
+                      StockDisplay(
+                        name: "Amazon",
+                        symbol: "AMZN",
+                        isUp: true,
+                        price: "\$132.00",
+                        growth: "9.054%",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "My portfolio",
+                      style: TextStyle(
+                        color: AppColors.primaryText,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                    InkWell(
+                      child: Text(
+                        "View all",
+                        style: TextStyle(
+                          color: AppColors.primaryText,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Transactions(
+                stockIcon: "assets/images/Amazon_icon.png",
+                stockName: "Amazon",
+                stockSymbol: "AMZN",
+                stockPrice: "\$132.00",
+                stockGrowth: "+9.054%",
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 0.5,
+          selectedItemColor: AppColors.contentColorBlue,
+          unselectedItemColor: AppColors.primaryText,
+          showUnselectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Iconsax.home),
+              label: "Home",
+              backgroundColor: AppColors.navBarBackground,
+            ),
+            BottomNavigationBarItem(icon: Icon(Iconsax.card), label: "Card"),
+            BottomNavigationBarItem(
+              icon: Icon(Iconsax.graph),
+              label: "Analytics",
+            ),
+            BottomNavigationBarItem(icon: Icon(Iconsax.user), label: "Profile"),
+          ],
+        ),
       ),
     );
   }

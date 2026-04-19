d985400c2329f3b0e06e00f4c0c8f45bf290b865
Aloamoh3 <aloamoh@icloud.com>
2026-04-19 07:37:21 -0400

    Update stock_point.dart

--- lib/models/stock_point.dart
+++ lib/models/stock_point.dart
@@ -1,1 +1,13 @@
+class StockPoint {
+  final DateTime time;
+  final double price;
 
+  StockPoint({required this.time, required this.price});
+
+  factory StockPoint.fromJson(Map<String, dynamic> json) {
+    return StockPoint(
+      time: DateTime.parse(json['time']),
+      price: json['price'].toDouble(),
+    );
+  }
+}
